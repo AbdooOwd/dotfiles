cc314c9aa25480ffb42cf2ea89353e211a89ecd1
AbdooOwd <benari102@gmail.com>
2025-09-12 03:24:27 +0100

    Automated backup of Awesome WM config on 2025-09-12_03:24:27

--- awesome/ui/bar/widgets/generic/popup.lua
+++ awesome/ui/bar/widgets/generic/popup.lua
@@ -1,6 +1,27 @@
 local awful = require("awful")
+local wibox = require("wibox")
 
 ---@param title string Pup-up's title
----@param popup_widget table Widget considered to be the pop-up
+---@param popup_widget table Widget considered to be the pop-up's content
 local function make_popup(title, popup_widget, parent_widget)
+    local popup = awful.popup {
+        widget = {
+            {
+                {
+                    text = title,
+                    widget = wibox.widget.textbox
+                },
+                popup_widget,
+                layout = wibox.layout.fixed.vertical
+            },
+            margins = 10,
+            widget = wibox.container.margin
+        },
+        placement = {},
+        ontop = true,
+        visible = false,
+        parent = parent_widget
+    }
+
+    return popup
 end
