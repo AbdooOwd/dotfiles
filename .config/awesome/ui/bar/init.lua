1481f5484a38acb8c3e056ffaa3485d0650af9d5
AbdooOwd <benari102@gmail.com>
2026-05-31 00:28:31 +0100

    wibar: tidier code?

--- .config/awesome/ui/bar/init.lua
+++ .config/awesome/ui/bar/init.lua
@@ -32,11 +32,11 @@ kbdcfg.widget = mykeyboardlayout
 local mytextclock = wibox.widget.textclock()
 
 -- TODO: move this elsewhere
-local separator = wibox.widget {
+--[[local separator = wibox.widget {
     markup = '<span font="' .. beautiful.get_font_height(beautiful.font) .. '"> | </span>',
     valign = "center",
     widget = wibox.widget.textbox
-}
+}]]
 
 
 -- widget sections
@@ -86,50 +86,52 @@ end
 
 
 awful.screen.connect_for_each_screen(function(s)
-    -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+  -- Each screen has its own tag table.
+  awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+  -- Create a promptbox for each screen
+  s.mypromptbox = awful.widget.prompt()
+  -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+  -- We need one layoutbox per screen.
+  s.mylayoutbox = awful.widget.layoutbox(s)
+  s.mylayoutbox:buttons(gears.table.join(
+                          awful.button({ }, 1, function () awful.layout.inc( 1) end),
+                          awful.button({ }, 3, function () awful.layout.inc(-1) end),
+                          awful.button({ }, 4, function () awful.layout.inc( 1) end),
+                          awful.button({ }, 5, function () awful.layout.inc(-1) end)))
 
-    -- sets the screen's systray
-    systray_module.set_screen_systray(s)
+  -- sets the screen's systray
+  systray_module.set_screen_systray(s)
 
-    -- Create the wibox
-    s.mywibox = awful.wibar({
-        position = "top",
-        screen = s,
-        height = 25,
-    })
+  -- Create the wibar
+  s.mywibar = awful.wibar({
+    position = "top",
+    screen = s,
+    height = beautiful.wibar_height,
+  })
 
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-      layout = wibox.layout.align.horizontal,
-      { -- Left Widgets
-        layout = wibox.layout.fixed.horizontal,
-        left_widgets(s),
-        s.mypromptbox,
-      },
-      middle_widgets(s),
-      { -- right widgets
-        right_widgets(s),
-        utils.margin_hv(
-          wibox.container.background(
-            wibox.container.margin(s.mylayoutbox, dpi(4), dpi(4), dpi(4), dpi(4)),
-            beautiful.bg_normal
-          ),
-          beautiful.margins.wibar.horizontal,
-          beautiful.margins.wibar.vertical
+  local wibar_widgets = {
+    layout = wibox.layout.align.horizontal,
+    { -- Left Widgets
+      layout = wibox.layout.fixed.horizontal,
+      left_widgets(s),
+      s.mypromptbox,
+    },
+    middle_widgets(s),
+    { -- right widgets
+      right_widgets(s),
+      utils.margin_hv(
+        wibox.container.background(
+          wibox.container.margin(s.mylayoutbox, 2, 2, 2, 2),
+          beautiful.bg_normal
         ),
-        layout = wibox.layout.fixed.horizontal
-      }
+        beautiful.margins.wibar.horizontal,
+        beautiful.margins.wibar.vertical
+      ),
+      layout = wibox.layout.fixed.horizontal
     }
+  }
+
+  -- Add widgets to the wibar
+  s.mywibar:setup(wibar_widgets)
 end)
